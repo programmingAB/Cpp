d3e8779d210d68d28ed730fd8180f95149c9b2c4
programmingAB <76556728+programmingAB@users.noreply.github.com>
2021-01-27 18:59:53 +0100

    Add files via upload

--- MathTrainerQT/mainwindow.cpp
+++ MathTrainerQT/mainwindow.cpp
@@ -2,26 +2,27 @@
 #include "ui_mainwindow.h"
 #include <QMessageBox>
 #include <QListWidgetItem>
+//#include <QKeyEvent>
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    ui->stackedWidget->setCurrentIndex(1);
+    ui->stackedWidget->setCurrentIndex(0);
 
     QListWidgetItem *item1 = new QListWidgetItem("Easy");
     item1->setTextAlignment(Qt::AlignCenter);
     ui->ListLevels->addItem(item1);
-    ui->ListLevels->addItem("Normal");
-    ui->ListLevels->addItem("Hard");
+    QListWidgetItem *item2 = new QListWidgetItem("Normal");
+    item2->setTextAlignment(Qt::AlignCenter);
+    ui->ListLevels->addItem(item2);
+    QListWidgetItem *item3 = new QListWidgetItem("Hard");
+    item3->setTextAlignment(Qt::AlignCenter);
+    ui->ListLevels->addItem(item3);
     ui->ListLevels->setCurrentRow(1);
     ui->ListLevels->setAcceptDrops(0);
-   // ui->ListLevels->setFlow(QListView::TopToBottom);
-    //ui->ListLevels->setTextAlignment(Qt::AlignCenter);
-
-
-    //ui->LstOperation->currentItem()
-}//
+    ui->label->setWordWrap(true);
+}
 
 MainWindow::~MainWindow()
 {
@@ -31,6 +32,7 @@ MainWindow::~MainWindow()
 
 void MainWindow::on_ButtonStart_clicked()
 {
+
     if (ui->InputName->text().isEmpty())
             ui->labelWarning->setText("Input your name please");
     else
--- MathTrainerQT/mainwindow.h
+++ MathTrainerQT/mainwindow.h
@@ -14,6 +14,7 @@ class MainWindow : public QMainWindow
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+    //void keyPressEvent(QKeyEvent * e);
 
 private slots:
     void on_ButtonStart_clicked();
